25f4b1d2006a5117d007efb698b764c02ea8b742
MagnusPC <magnuspc@hotmail.dk>
2025-01-30 16:02:37 +0100

    eod

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ based on
 
 
 https://github.com/florian1345/sudoku-variants/tree/master
+
+
+trying to learn rust so i can make a similar puzzle (Tents and Trees), also in rust
--- rust_puzzle/src/resource_lib.rs
+++ rust_puzzle/src/resource_lib.rs
@@ -1,3 +1,23 @@
+pub mod constraint; //folder
+pub mod error; //file
+pub mod generator; //file
+pub mod solver; //folder
+pub mod util; //file
+
+#[cfg(test)]
+mod fix_tests;
+
+#[cfg(test)]
+mod random_tests;
+
+use constraint::Constraint;
+use error::{SudokuError, SudokuParseError, SudokuParseResult, SudokuResult};
+
+use serde::{Deserialize, Serialize};
+use serde::de::{self, Deserializer, MapAccess, SeqAccess, Visitor};
+
+use std::fmt::{self, Display, Error, Formatter};
+
 // main grid struct
 #[derive(Clone, Debug, Eq, PartialEq, Serialize)]
 pub struct SudokuGrid {
@@ -26,4 +46,16 @@ fn build_sudoku_grid<E: de::Error>(
 
     grid.cells = cells;
     Ok(grid)
+}
+
+fn read_field<'de, M, V>(buffer: &mut Option<V>, map: &mut M, field_name: &'static str) -> Result<(), M::Error> where
+    M: MapAccess<'de>,
+    V: Deserialize<'de>{
+            if buffer.is_some() {
+                Err(<M::Error as de::Error>::duplicate_field(field_name))
+            }
+            else {
+                *buffer = Some(map.next_value()?);
+                Ok(())
+            }
 }
