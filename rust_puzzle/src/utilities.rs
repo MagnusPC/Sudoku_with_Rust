db1ce5c40d8b3559ada845c96b5a1cd4f03dd5f5
MagnusPC <magnuspc@hotmail.dk>
2025-02-22 15:59:12 +0100

    bump

--- rust_puzzle/src/utilities.rs
+++ rust_puzzle/src/utilities.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use std::hash::Hash;
-use std::mem;
+use std::{iter, mem};
 use std::ops::{
     BitAnd, BitAndAssign, BitOr, BitOrAssign, BitXor, BitXorAssign, Not, Sub, SubAssign,
 };
@@ -363,5 +363,626 @@ impl USizeSet {
         }
     }
 
-    //line 622
+    pub fn is_disjoint(&self, other: &USizeSet) -> USizeSetResult<bool> {
+        self.rel(other, |s, o| s & o == 0)
+    }
+
+    pub fn is_subset(&self, other: &USizeSet) -> USizeSetResult<bool> {
+        self.rel(other, |s, o| s & o == s)
+    }
+
+    pub fn is_proper_subset(&self, other: &USizeSet) -> USizeSetResult<bool> {
+        Ok(self.is_subset(other)? && self.len < other.len)
+    }
+
+    pub fn is_superset(&self, other: &USizeSet) -> USizeSetResult<bool> {
+        other.is_subset(self)
+    }
+
+    pub fn is_proper_superset(&self, other: &USizeSet) -> USizeSetResult<bool> {
+        other.is_proper_subset(self)
+    }
+}
+
+#[macro_export]
+macro_rules! set {
+    ($set:expr; $e:expr) => {
+        ($set).insert($e).unwrap()
+    };
+
+    ($set:expr; $e:expr, $($es:expr),+) => {
+        set!($set; $e);
+        set!($set; $($es),+)
+    };
+
+    ($lower:expr, $upper:expr; $($es:expr),+) => {
+        {
+            let mut set = USizeSet::new($lower, $upper).unwrap();
+            set!(set; $($es),+);
+            set
+        }
+    };
+}
+
+impl BitAnd<&USizeSet> for USizeSet {
+    type Output = USizeSet;
+
+    fn bitand(mut self, rhs: &USizeSet) -> USizeSet {
+        self.intersect_assign(rhs).unwrap();
+        self
+    }
+}
+
+impl BitOr<&USizeSet> for USizeSet {
+    type Output = USizeSet;
+
+    fn bitor(mut self, rhs: &USizeSet) -> USizeSet {
+        self.union_assign(rhs).unwrap();
+        self
+    }
+}
+
+impl Sub<&USizeSet> for USizeSet {
+    type Output = USizeSet;
+
+    fn sub(mut self, rhs: &USizeSet) -> USizeSet {
+        self.difference_assign(rhs).unwrap();
+        self
+    }
+}
+
+impl BitXor<&USizeSet> for USizeSet {
+    type Output = USizeSet;
+
+    fn bitxor(mut self, rhs: &USizeSet) -> USizeSet {
+        self.symmetric_difference_assign(rhs).unwrap();
+        self
+    }
+}
+
+impl BitAnd for &USizeSet {
+    type Output = USizeSet;
+
+    fn bitand(self, rhs: Self) -> USizeSet {
+        self.intersect(rhs).unwrap()
+    }
+}
+
+impl BitOr for &USizeSet {
+    type Output = USizeSet;
+
+    fn bitor(self, rhs: Self) -> USizeSet {
+        self.union(rhs).unwrap()
+    }
+}
+
+impl Sub for &USizeSet {
+    type Output = USizeSet;
+
+    fn sub(self, rhs: Self) -> USizeSet {
+        self.difference(rhs).unwrap()
+    }
+}
+
+impl BitXor for &USizeSet {
+    type Output = USizeSet;
+
+    fn bitxor(self, rhs: Self) -> USizeSet {
+        self.symmetric_difference(rhs).unwrap()
+    }
+}
+
+impl BitAndAssign<&USizeSet> for USizeSet {
+    fn bitand_assign(&mut self, rhs: &USizeSet) {
+        self.intersect_assign(rhs).unwrap();
+    }
+}
+
+impl BitOrAssign<&USizeSet> for USizeSet {
+    fn bitor_assign(&mut self, rhs: &USizeSet) {
+        self.union_assign(rhs).unwrap();
+    }
+}
+
+impl SubAssign<&USizeSet> for USizeSet {
+    fn sub_assign(&mut self, rhs: &USizeSet) {
+        self.difference_assign(rhs).unwrap();
+    }
+}
+
+impl BitXorAssign<&USizeSet> for USizeSet {
+    fn bitxor_assign(&mut self, rhs: &USizeSet) {
+        self.symmetric_difference_assign(rhs).unwrap();
+    }
+}
+
+impl BitAndAssign<&USizeSet> for &mut USizeSet {
+    fn bitand_assign(&mut self, rhs: &USizeSet) {
+        self.intersect_assign(rhs).unwrap();
+    }
+}
+
+impl BitOrAssign<&USizeSet> for &mut USizeSet {
+    fn bitor_assign(&mut self, rhs: &USizeSet) {
+        self.union_assign(rhs).unwrap();
+    }
+}
+
+impl SubAssign<&USizeSet> for &mut USizeSet {
+    fn sub_assign(&mut self, rhs: &USizeSet) {
+        self.difference_assign(rhs).unwrap();
+    }
+}
+
+impl BitXorAssign<&USizeSet> for &mut USizeSet {
+    fn bitxor_assign(&mut self, rhs: &USizeSet) {
+        self.symmetric_difference_assign(rhs).unwrap();
+    }
+}
+
+impl Not for &USizeSet {
+    type Output = USizeSet;
+
+    fn not(self) -> Self::Output {
+        self.complement()
+    }
+}
+
+impl Not for USizeSet {
+    type Output = USizeSet;
+
+    fn not(mut self) -> USizeSet {
+        self.complement_assign();
+        self
+    }
+}
+
+pub(crate) fn contains_duplicate<I>(mut iter: I) -> bool 
+where 
+    I: Iterator, 
+    I::Item: Hash + Eq 
+    {
+    let mut set = HashSet::new();
+    iter.any(|e| !set.insert(e))
+}
+
+pub(crate) fn abs_diff(a: usize, b: usize) -> usize {
+    if a < b {
+        b - a
+    }
+    else {
+        a - b
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_set_is_empty() {
+        let set = USizeSet::new(1, 9).unwrap();
+        assert!(set.is_empty());
+        assert!(!set.contains(1));
+        assert!(!set.contains(3));
+        assert!(!set.contains(9));
+        assert_eq!(0, set.len());
+    }
+
+    #[test]
+    fn range_set_is_full() {
+        let set = USizeSet::range(1, 9).unwrap();
+        assert!(!set.is_empty());
+        assert!(set.contains(1));
+        assert!(set.contains(3));
+        assert!(set.contains(9));
+        assert_eq!(9, set.len());
+    }
+
+    #[test]
+    fn multi_word_range() {
+        let set = USizeSet::range(100, 199).unwrap();
+        assert!(set.contains(100));
+        assert!(set.contains(199));
+        assert!(!set.contains(99));
+        assert!(!set.contains(200));
+        assert_eq!(100, set.len());
+    }
+
+    #[test]
+    fn singleton_set_contains_only_given_element() {
+        let set = USizeSet::singleton(1, 9, 3).unwrap();
+        assert!(!set.is_empty());
+        assert!(!set.contains(1));
+        assert!(set.contains(3));
+        assert!(!set.contains(9));
+        assert_eq!(1, set.len());
+    }
+
+    #[test]
+    fn set_macro_has_specified_range() {
+        let set = set!(2, 5; 3);
+        assert_eq!(2, set.lower());
+        assert_eq!(5, set.upper());
+    }
+
+    #[test]
+    fn set_macro_contains_specified_elements() {
+        let set = set!(2, 8; 3, 7, 8);
+        assert_eq!(3, set.len());
+        assert!(set.contains(3));
+        assert!(set.contains(7));
+        assert!(set.contains(8));
+        assert!(!set.contains(5));
+    }
+
+    #[test]
+    fn set_creation_error() {
+        assert_eq!(Err(USizeSetError::InvalidBounds), USizeSet::new(1, 0));
+        assert_eq!(Err(USizeSetError::InvalidBounds), USizeSet::new(5, 3));
+    }
+
+    #[test]
+    fn set_insertion_error() {
+        let mut set = USizeSet::new(1, 5).unwrap();
+        assert_eq!(Err(USizeSetError::OutOfBounds), set.insert(0));
+        assert_eq!(Err(USizeSetError::OutOfBounds), set.insert(6));
+    }
+
+    #[test]
+    fn set_operation_error() {
+        let set_1 = USizeSet::new(1, 9).unwrap();
+        let set_2 = USizeSet::new(1, 6).unwrap();
+        assert_eq!(Err(USizeSetError::DifferentBounds), set_1.union(&set_2));
+        assert_eq!(Err(USizeSetError::DifferentBounds), set_2.intersect(&set_1));
+    }
+
+    #[test]
+    fn manipulation() {
+        let mut set = USizeSet::new(1, 9).unwrap();
+        set.insert(2).unwrap();
+        set.insert(4).unwrap();
+        set.insert(6).unwrap();
+
+        assert!(!set.is_empty());
+        assert!(set.contains(2));
+        assert!(set.contains(4));
+        assert!(set.contains(6));
+        assert_eq!(3, set.len());
+
+        set.remove(4).unwrap();
+
+        assert!(!set.is_empty());
+        assert!(set.contains(2));
+        assert!(!set.contains(4));
+        assert!(set.contains(6));
+        assert_eq!(2, set.len());
+
+        set.clear();
+
+        assert!(set.is_empty());
+        assert!(!set.contains(2));
+        assert!(!set.contains(4));
+        assert!(!set.contains(6));
+        assert_eq!(0, set.len());
+    }
+
+    #[test]
+    fn iteration() {
+        let mut set = USizeSet::new(1, 100).unwrap();
+        set.insert(1).unwrap();
+        set.insert(12).unwrap();
+        set.insert(23).unwrap();
+        set.insert(36).unwrap();
+        set.insert(42).unwrap();
+        set.insert(64).unwrap();
+        set.insert(65).unwrap();
+        set.insert(97).unwrap();
+        set.insert(100).unwrap();
+
+        let mut iter = set.iter();
+
+        assert_eq!(Some(1), iter.next());
+        assert_eq!(Some(12), iter.next());
+        assert_eq!(Some(23), iter.next());
+        assert_eq!(Some(36), iter.next());
+        assert_eq!(Some(42), iter.next());
+        assert_eq!(Some(64), iter.next());
+        assert_eq!(Some(65), iter.next());
+        assert_eq!(Some(97), iter.next());
+        assert_eq!(Some(100), iter.next());
+        assert_eq!(None, iter.next());
+    }
+
+    #[test]
+    fn double_insert() {
+        let mut set = USizeSet::new(1, 9).unwrap();
+        assert!(set.insert(3).unwrap());
+        assert!(set.insert(4).unwrap());
+        assert!(!set.insert(3).unwrap());
+
+        assert!(set.contains(3));
+        assert_eq!(2, set.len());
+    }
+
+    #[test]
+    fn double_remove() {
+        let mut set = USizeSet::range(1, 9).unwrap();
+        assert!(set.remove(3).unwrap());
+        assert!(set.remove(5).unwrap());
+        assert!(!set.remove(3).unwrap());
+
+        assert!(!set.contains(3));
+        assert_eq!(7, set.len());
+    }
+
+    fn op_test_lhs() -> USizeSet {
+        set!(1, 4; 2, 4)
+    }
+
+    fn op_test_rhs() -> USizeSet {
+        set!(1, 4; 3, 4)
+    }
+
+    fn triangle_nums_to_100() -> USizeSet {
+        set!(1, 100; 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78, 91)
+    }
+
+    fn fibs_to_100() -> USizeSet {
+        set!(1, 100; 1, 2, 3, 5, 8, 13, 21, 34, 55, 89)
+    }
+
+    #[test]
+    fn union() {
+        let result = op_test_lhs() | &op_test_rhs();
+        let expected = set!(1, 4; 2, 3, 4);
+        assert_eq!(expected, result);
+        assert_eq!(3, result.len());
+    }
+
+    #[test]
+    fn multi_word_union() {
+        let result = triangle_nums_to_100() | &fibs_to_100();
+        let expected = set!(1, 100;
+            1, 2, 3, 5, 6, 8, 10, 13, 15, 21,
+            28, 34, 36, 45, 55, 66, 78, 89, 91);
+        assert_eq!(expected, result);
+        assert_eq!(19, result.len());
+    }
+
+    #[test]
+    fn intersection() {
+        let result = op_test_lhs() & &op_test_rhs();
+        let expected = set!(1, 4; 4);
+        assert_eq!(expected, result);
+        assert_eq!(1, result.len());
+    }
+
+    #[test]
+    fn multi_word_intersection() {
+        let result = triangle_nums_to_100() & &fibs_to_100();
+        let expected = set!(1, 100; 1, 3, 21, 55);
+        assert_eq!(expected, result);
+        assert_eq!(4, result.len())
+    }
+
+    #[test]
+    fn difference() {
+        let result = op_test_lhs() - &op_test_rhs();
+        let expected = set!(1, 4; 2);
+        assert_eq!(expected, result);
+        assert_eq!(1, result.len());
+    }
+
+    #[test]
+    fn multi_word_difference() {
+        let result = triangle_nums_to_100() - &fibs_to_100();
+        let expected = set!(1, 100; 6, 10, 15, 28, 36, 45, 66, 78, 91);
+        assert_eq!(expected, result);
+        assert_eq!(9, result.len());
+    }
+
+    #[test]
+    fn symmetric_difference() {
+        let result = op_test_lhs() ^ &op_test_rhs();
+        let expected = set!(1, 4; 2, 3);
+        assert_eq!(expected, result);
+        assert_eq!(2, result.len());
+    }
+
+    #[test]
+    fn multi_word_symmetric_difference() {
+        let result = triangle_nums_to_100() ^ &fibs_to_100();
+        let expected =
+            set!(1, 100;
+                2, 5, 6, 8, 10, 13, 15, 28, 34, 36, 45, 66, 78, 89, 91);
+        assert_eq!(expected, result);
+        assert_eq!(15, result.len());
+    }
+
+    #[test]
+    fn complement() {
+        let result = !op_test_lhs();
+        let expected = set!(1, 4; 1, 3);
+        assert_eq!(expected, result);
+        assert_eq!(2, result.len());
+    }
+
+    #[test]
+    fn multi_word_complement() {
+        let result = !triangle_nums_to_100();
+        let mut expected = USizeSet::range(1, 100).unwrap();
+
+        for i in 1..=13 {
+            expected.remove(i * (i + 1) / 2).unwrap();
+        }
+
+        assert_eq!(expected, result);
+        assert_eq!(87, result.len());
+    }
+
+    #[test]
+    fn complement_full() {
+        let result = !USizeSet::range(5, 105).unwrap();
+        let expected = USizeSet::new(5, 105).unwrap();
+        assert_eq!(expected, result);
+        assert_eq!(0, result.len());
+    }
+
+    #[test]
+    fn complement_empty() {
+        let result = !USizeSet::new(5, 105).unwrap();
+        let expected = USizeSet::range(5, 105).unwrap();
+        assert_eq!(expected, result);
+        assert_eq!(101, result.len());
+    }
+
+    #[test]
+    fn contains_duplicate_false() {
+        let vec = vec![1, 5, 2, 4, 3];
+        assert!(!contains_duplicate(vec.iter()));
+        assert!(!contains_duplicate(vec.iter().map(|i| i.to_string())));
+    }
+
+    #[test]
+    fn contains_duplicate_true() {
+        let vec = vec![1, 5, 2, 4, 5];
+        assert!(contains_duplicate(vec.iter()));
+        assert!(contains_duplicate(vec.iter().map(|i| i.to_string())));
+    }
+
+    #[test]
+    fn min_empty() {
+        assert_eq!(None, USizeSet::new(512, 1024).unwrap().min());
+    }
+
+    #[test]
+    fn min_filled() {
+        assert_eq!(Some(2), set!(1, 9; 2, 5).min());
+        assert_eq!(Some(100), set!(1, 200; 100, 105, 195).min());
+    }
+
+    #[test]
+    fn max_empty() {
+        assert_eq!(None, USizeSet::new(512, 1024).unwrap().max());
+    }
+
+    #[test]
+    fn max_filled() {
+        assert_eq!(Some(5), set!(1, 9; 2, 5).max());
+        assert_eq!(Some(100), set!(1, 200; 5, 95, 100).max());
+    }
+
+    #[test]
+    fn disjoint_relations() {
+        let primes = set!(1, 10; 2, 3, 5, 7);
+        let squares = set!(1, 10; 1, 4, 9);
+        let even = set!(1, 10; 2, 4, 6, 8, 10);
+
+        assert!(primes.is_disjoint(&squares).unwrap());
+        assert!(!primes.is_disjoint(&even).unwrap());
+    }
+
+    #[test]
+    fn multi_word_disjoint_relations() {
+        let fibs = fibs_to_100();
+        let squares = set!(1, 100; 1, 4, 9, 16, 25, 36, 49, 64, 81, 100);
+        let big_squares = set!(1, 100; 4, 9, 16, 25, 36, 49, 64, 81, 100);
+        let singleton_89 = USizeSet::singleton(1, 100, 89).unwrap();
+
+        assert!(!fibs.is_disjoint(&squares).unwrap());
+        assert!(fibs.is_disjoint(&big_squares).unwrap());
+        assert!(!fibs.is_disjoint(&singleton_89).unwrap());
+    }
+
+    fn assert_subset(a: &USizeSet, b: &USizeSet) {
+        assert!(a.is_subset(b).unwrap());
+        assert!(b.is_superset(a).unwrap());
+    }
+
+    fn assert_not_subset(a: &USizeSet, b: &USizeSet) {
+        assert!(!a.is_subset(b).unwrap());
+        assert!(!b.is_superset(a).unwrap());
+    }
+
+    fn subset_test_sets() -> (USizeSet, USizeSet, USizeSet, USizeSet) {
+        (
+            set!(1, 10; 2, 3, 5, 7),
+            set!(1, 10; 3, 5, 7),
+            set!(1, 10; 1, 4, 9),
+            set!(1, 10; 1, 4, 9)
+        )
+    }
+
+    fn multi_word_subset_test_sets() -> (USizeSet, USizeSet, USizeSet, USizeSet, USizeSet) {
+        (
+            set!(1, 100; 20, 30, 50, 70),
+            set!(1, 100; 30, 50, 70),
+            set!(1, 100; 20, 30, 50),
+            set!(1, 100; 10, 40, 90),
+            set!(1, 100; 10, 40, 90)
+        )
+    }
+
+    #[test]
+    fn subset_relations() {
+        let (a, b, c, d) = subset_test_sets();
+
+        assert_not_subset(&a, &b);
+        assert_subset(&b, &a);
+        assert_not_subset(&a, &c);
+        assert_not_subset(&c, &a);
+        assert_subset(&c, &d);
+        assert_subset(&d, &c);
+    }
+
+    #[test]
+    fn multi_word_subset_relations() {
+        let (a, b, c, d, e) = multi_word_subset_test_sets();
+
+        assert_not_subset(&a, &b);
+        assert_subset(&b, &a);
+        assert_not_subset(&a, &c);
+        assert_subset(&c, &a);
+        assert_not_subset(&a, &d);
+        assert_not_subset(&d, &a);
+        assert_subset(&d, &e);
+        assert_subset(&e, &d);
+    }
+
+    fn assert_proper_subset(a: &USizeSet, b: &USizeSet) {
+        assert!(a.is_proper_subset(b).unwrap());
+        assert!(b.is_proper_superset(a).unwrap());
+    }
+
+    fn assert_not_proper_subset(a: &USizeSet, b: &USizeSet) {
+        assert!(!a.is_proper_subset(b).unwrap());
+        assert!(!b.is_proper_superset(a).unwrap());
+    }
+
+    #[test]
+    fn proper_subset_relations() {
+        let (a, b, c, d) = subset_test_sets();
+
+        assert_not_proper_subset(&a, &b);
+        assert_proper_subset(&b, &a);
+        assert_not_proper_subset(&a, &c);
+        assert_not_proper_subset(&c, &a);
+        assert_not_proper_subset(&c, &d);
+        assert_not_proper_subset(&d, &c);
+    }
+
+    #[test]
+    fn multi_word_proper_subset_relations() {
+        let (a, b, c, d, e) = multi_word_subset_test_sets();
+
+        assert_not_proper_subset(&a, &b);
+        assert_proper_subset(&b, &a);
+        assert_not_proper_subset(&a, &c);
+        assert_proper_subset(&c, &a);
+        assert_not_proper_subset(&a, &d);
+        assert_not_proper_subset(&d, &a);
+        assert_not_proper_subset(&d, &e);
+        assert_not_proper_subset(&e, &d);
+    }
 }
