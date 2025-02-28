b038d61372f00de36c6c9a04a711372bac550736
MagnusPC <103988876+MagnusPC@users.noreply.github.com>
2025-02-28 11:55:53 +0100

    bump, dependencies

--- rust_puzzle/src/sudoku_generator.rs
+++ rust_puzzle/src/sudoku_generator.rs
@@ -1,20 +1,20 @@
 // generate the sudokus
 // aka main programme
 
-use crate::{Sudoku, SudokuGrid};
 use crate::constraint::{reducible, Constraint};
 use crate::error::{SudokuError, SudokuResult};
 use crate::solver::{BacktrackingSolver, Solution, Solver};
+use crate::{Sudoku, SudokuGrid};
 
-use rand::Rng;
 use rand::rngs::ThreadRng;
+use rand::Rng;
 
 use rand_distr::Normal;
 
 use std::f64::consts;
 
-pub struct Generator<R: Rng>{
-    rng: R
+pub struct Generator<R: Rng> {
+    rng: R,
 }
 
 impl Generator<ThreadRng> {
@@ -37,12 +37,15 @@ pub(crate) fn shuffle<T>(rng: &mut impl Rng, values: impl Iterator<Item = T>) ->
 
 impl<R: Rng> Generator<R> {
     pub fn new(rng: R) -> Generator<R> {
-        Generator {
-            rng
-        }
+        Generator { rng }
     }
 
-    fn fill_rec<C: Constraint + Clone>(&mut self, sudoku: &mut Sudoku<C>, column: usize, row: usize) -> {
+    fn fill_rec<C: Constraint + Clone>(
+        &mut self,
+        sudoku: &mut Sudoku<C>,
+        column: usize,
+        row: usize,
+    ) -> bool {
         let size = sudoku.grid().size();
 
         if row == size {
@@ -50,7 +53,7 @@ impl<R: Rng> Generator<R> {
         }
 
         let next_column = (column + 1) % size;
-        let next_row = if next_column == 0 { row + 1} else {row};
+        let next_row = if next_column == 0 { row + 1 } else { row };
 
         if sudoku.grid().get_cell(column, row).unwrap().is_some() {
             return self.fill_rec(sudoku, next_column, next_row);
@@ -70,16 +73,26 @@ impl<R: Rng> Generator<R> {
         false
     }
 
-    pub fn fill<C>(&mut self, sudoku: &mut Sudoku<C>) -> SudokuResult<()> where C: Constraint + Clone {
+    pub fn fill<C>(&mut self, sudoku: &mut Sudoku<C>) -> SudokuResult<()>
+    where
+        C: Constraint + Clone,
+    {
         if self.fill_rec(sudoku, 0, 0) {
             Ok(())
-        }
-        else {
+        } else {
             Err(SudokuError::UnsatisfiableConstraint)
         }
     }
 
-    pub fn generate<C>(&mut self, block_width: usize, block_height: usize, constraint: C) -> SudokuResult<Sudoku<C>> where C: Constraint + Clone {
+    pub fn generate<C>(
+        &mut self,
+        block_width: usize,
+        block_height: usize,
+        constraint: C,
+    ) -> SudokuResult<Sudoku<C>>
+    where
+        C: Constraint + Clone,
+    {
         let mut sudoku = Sudoku::new_empty(block_width, block_height, constraint)?;
         self.fill(&mut sudoku)?;
         Ok(sudoku)
@@ -106,7 +119,7 @@ impl<R, F: Fn(&R) -> f64> ReductionPrioritizer<R> for F {
 
 pub struct Reducer<S: Solver, R: Rng> {
     solver: S,
-    rng: R
+    rng: R,
 }
 
 impl Reducer<BacktrackingSolver, ThreadRng> {
@@ -116,35 +129,34 @@ impl Reducer<BacktrackingSolver, ThreadRng> {
 }
 
 pub enum Reduction<R> {
-    RemoveDigit {
-        column: usize,
-        row: usize
-    },
+    RemoveDigit { column: usize, row: usize },
 
-    ReduceConstraint {
-        reduction: R
-    }
+    ReduceConstraint { reduction: R },
 }
 
 impl<R> Reduction<R> {
-    fn apply<S, C>(&self, sudoku: &mut Sudoku<C>, solution: &SudokuGrid, solver: &S) where S: Solver, C: Constraint<Reduction = R> + Clone + 'static {
+    fn apply<S, C>(&self, sudoku: &mut Sudoku<C>, solution: &SudokuGrid, solver: &S)
+    where
+        S: Solver,
+        C: Constraint<Reduction = R> + Clone + 'static,
+    {
         match self {
             Reduction::RemoveDigit { column, row } => {
                 let number = sudoku.grid().get_cell(*column, *row).unwrap().unwrap();
                 sudoku.grid_mut().clear_cell(*column, *row).unwrap();
 
-                if let Solution::Unique(_) = solver.solve(sudoku) { }
-                else {
+                if let Solution::Unique(_) = solver.solve(sudoku) {
+                } else {
                     sudoku.grid_mut().set_cell(*column, *row, number).unwrap();
                 }
-            },
-            Reduction::ReduceConstraint { reduction: r} => {
+            }
+            Reduction::ReduceConstraint { reduction: r } => {
                 let constraint = sudoku.constraint_mut();
                 let reduce_res = constraint.reduce(solution, r);
 
                 if let Ok(revert_info) = reduce_res {
-                    if let Solution::Unique(_) = solver.solve(sudoku) { }
-                    else {
+                    if let Solution::Unique(_) = solver.solve(sudoku) {
+                    } else {
                         let constraint = sudoku.constraint_mut();
                         constraint.revert(solution, r, revert_info);
                     }
@@ -154,7 +166,10 @@ impl<R> Reduction<R> {
     }
 }
 
-fn reduction<R, C>(sudoku: &Sudoku<C>) -> impl Iterator<Item = Reduction<R>> where C: Constraint<Reduction = R> + Clone {
+fn reduction<R, C>(sudoku: &Sudoku<C>) -> impl Iterator<Item = Reduction<R>>
+where
+    C: Constraint<Reduction = R> + Clone,
+{
     let size = sudoku.grid().size();
     //line 281
 }
